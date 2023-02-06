20e820d8b82e62223545e7c5480031af753e250c
nikks1902 <nikoreal1902@gmail.com>
2023-02-06 15:19:22 +0300

    intro test

---
+++ src/IntroClass.java
@@ -0,0 +1,8 @@
+public class IntroClass {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 5;
+        int z = 100 + 10 * -20 / x * (y-1);
+        System.out.println(z);
+    }
+}
