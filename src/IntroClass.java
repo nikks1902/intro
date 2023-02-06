fd025e5c365931f92a2297aeaf723f663afaedce
nikks1902 <nikoreal1902@gmail.com>
2023-02-06 16:26:17 +0300

    Changes

--- src/IntroClass.java
+++ src/IntroClass.java
@@ -1,6 +1,6 @@
 public class IntroClass {
     public static void main(String[] args) {
-        int x = 10;
+        int x = 11;
         int y = 5;
         int z = 100 - 10 * -20 / x * (y+1);
         System.out.println(z);
